13b599fe814ab816ab57bad3733b57432e24fec5
Alvaro Somoza <somoza.alvaro@gmail.com>
2025-12-28 12:02:54 -0300

    more dimensions variations

--- src/iartisanz/modules/generation/widgets/image_dimensions_widget.py
+++ src/iartisanz/modules/generation/widgets/image_dimensions_widget.py
@@ -6,30 +6,86 @@ from iartisanz.app.event_bus import EventBus
 
 ALLOWED_VALUES = [
     512,
+    544,
     576,
+    608,
     640,
+    672,
     704,
+    736,
     768,
+    800,
     832,
+    864,
     896,
+    928,
     960,
+    992,
     1024,
+    1056,
     1088,
+    1120,
     1152,
+    1184,
     1216,
+    1248,
     1280,
+    1312,
     1344,
+    1376,
     1408,
+    1440,
     1472,
+    1504,
     1536,
+    1568,
     1600,
+    1632,
     1664,
+    1696,
     1728,
+    1760,
     1792,
+    1824,
     1856,
+    1888,
     1920,
+    1952,
     1984,
+    2016,
     2048,
+    2080,
+    2112,
+    2144,
+    2176,
+    2208,
+    2240,
+    2272,
+    2304,
+    2336,
+    2368,
+    2400,
+    2432,
+    2464,
+    2496,
+    2528,
+    2560,
+    2592,
+    2624,
+    2656,
+    2688,
+    2720,
+    2752,
+    2784,
+    2816,
+    2848,
+    2880,
+    2912,
+    2944,
+    2976,
+    3008,
+    3040,
+    3072,
 ]
 
 
@@ -50,7 +106,7 @@ class ImageDimensionsWidget(QWidget):
         image_sliders_layout.addWidget(width_label, 0, 0)
 
         self.width_slider = QSlider()
-        self.width_slider.setRange(512, 2048)
+        self.width_slider.setRange(512, 3072)
         self.width_slider.setSingleStep(1)
         self.width_slider.setPageStep(1)
         self.width_slider.setOrientation(Qt.Orientation.Horizontal)
@@ -63,7 +119,7 @@ class ImageDimensionsWidget(QWidget):
         image_sliders_layout.addWidget(height_label, 1, 0)
 
         self.height_slider = QSlider()
-        self.height_slider.setRange(512, 2048)
+        self.height_slider.setRange(512, 3072)
         self.height_slider.setSingleStep(1)
         self.height_slider.setPageStep(1)
         self.height_slider.setOrientation(Qt.Orientation.Horizontal)
